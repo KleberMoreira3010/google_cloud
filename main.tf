d9b910f881443c307ef27dcc151821ca14bf436e
KleberMoreira3010 <klb_moreira@hotmail.com>
2022-12-11 00:13:36 -0300

    TESTE

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "gcs_bucket" {
     name="test-bucket-random-001123"
+    location = var.region
 
 }
--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 terraform {
   backend "gcs" {
-    bucket = "stack-state-gcs-data-pipeline687568765"
+    bucket = "stack-state-gcs-data-pipeline6875687651"
     prefix = "terraform/state"
   }
   required_providers {
